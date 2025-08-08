933533c155caf622fed0ce0544506ea41588dfbe
TTifanioh <167166956+TTifanioh@users.noreply.github.com>
2025-08-08 12:38:41 +0300

    Update Trace_histograme.py

--- Trace_histograme.py
+++ Trace_histograme.py
@@ -1,7 +1,7 @@
 L=[2, 2, 6, 6, 3, 14, 14, 6, 8, 9, 9]
 import matplotlib.piplot as plt
 plt.figure()
-plt.hist(L, bins=1é, color='red')
+plt.hist(L, bins=12, color='red')
 plt.title("Histogramme de la liste L")
 plt.xlabel("Valeurs de L")
 plt.ylabel("Nombre d'occurrences")
