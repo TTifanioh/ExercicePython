dbc77df2fb6f7e344240c06753af1fe773c7833e
TTifanioh <167166956+TTifanioh@users.noreply.github.com>
2025-08-12 10:45:22 +0300

    Create calcul_skeweness.py

--- src/calcul_skeweness.py
+++ src/calcul_skeweness.py
@@ -1,2 +1,11 @@
 import numpy as np
-import stats_util.py as 
+import stats_util as rec_moy,rec_variance,rec_ecart_type
+def skeweness(height_list):
+  n=len(height_list)
+  et3=rec_ecart_type(height_list)**3
+  moyenne=rec_moy(height_list)
+  S=0
+  for i in range (n):
+    S+=(height_list[i]-moyenne)**3
+    S= (n/(n-1)/(n-2))*S/et3
+  return S
